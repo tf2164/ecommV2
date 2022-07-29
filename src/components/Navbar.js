7a87609fb77582371b5f8147864e08304ee32612
TyraR2H <tyrar2h@AdminisatorsMBP.home>
2022-07-29 11:59:18 -0400

    vaildate css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,15 @@
-
+import Hamburger from 'hamburger-react';
 import './pages.css'
+import React, {useState} from 'react';
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
+
+
 export default function Navbar() {
+
+  const [isOpen, setOpen] = useState(false)
+
+
   return (
     <nav className="nav">
       <Link to="/" className="ecommerce">
@@ -13,8 +20,29 @@ export default function Navbar() {
         <CustomLink to="/Contact">Contact</CustomLink>
       </ul>
 
-      
+      {/* <Hamburger  direction="right" duration={0.8} toggled={isOpen} toggle={setOpen}>
+      <ul>
+        <CustomLink to="/Products">Products</CustomLink>
+        <CustomLink to="/Contact">Contact</CustomLink>
+      </ul>
+      </Hamburger> */}
+
+      <Hamburger id="sidebar" class="sidebar" direction="right" duration={0.8} onToggle={toggled => {
+  if (toggled) {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+
+  } else {
+     document.getElementById("sidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  }
+}} />
     </nav>
+
+
+
+
+
   );
 
 //   /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
